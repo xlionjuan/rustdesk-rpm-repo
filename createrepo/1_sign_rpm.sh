e2e5dcdc7fce612fa5abdb3e2d0a42eeaa891624
XLion <xlion@xlion.tw>
2025-11-14 22:59:45 +0800

    Add RPM sign check but no retry

--- createrepo/1_sign_rpm.sh
+++ createrepo/1_sign_rpm.sh
@@ -2,7 +2,18 @@
 
 set -oue pipefail
 
+echo "Signing..."
 rpm --addsign wwwroot/latest/*.rpm & \
 rpm --addsign wwwroot/latest-suse/*.rpm & \
 rpm --addsign wwwroot/nightly/*.rpm & \
 rpm --addsign wwwroot/nightly-suse/*.rpm
+echo ""
+
+echo "Validate..."
+
+set -ouex pipefail
+
+rpm -K wwwroot/latest/*.rpm | grep -q "signatures OK"
+rpm -K wwwroot/latest-suse/*.rpm | grep -q "signatures OK"
+rpm -K wwwroot/nightly/*.rpm | grep -q "signatures OK"
+rpm -K wwwroot/nightly-suse/*.rpm | grep -q "signatures OK"
